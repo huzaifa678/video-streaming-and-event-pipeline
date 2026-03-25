b7244180416a8b28a83812e157070f736be91b78
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-03-25 22:22:45 +0500

    remived duplicate slashes in build.sh

--- build.sh
+++ build.sh
@@ -18,22 +18,16 @@ clang++ ../video-streaming-service/capture.cpp -std=c++17 \
   -I$SDK_PATH/dependency/libkvscproducer/kvscproducer-src/dependency/libkvspic/kvspic-src/src/trace/include \
   -I$SDK_PATH/dependency/libkvscproducer/kvscproducer-src/dependency/libkvspic/kvspic-src/src/signaling/include \
   -I$SDK_PATH/dependency/libkvscproducer/kvscproducer-src/dependency/libkvspic/kvspic-src/src/streaming/include \
-  \
   $(pkg-config --cflags libavcodec libavutil libavformat opencv4) \
-  \
   -L$SDK_PATH/build/dependency/libkvscproducer/kvscproducer-src \
   -L$SDK_PATH/open-source/local/lib \
-  \
   -Wl,-rpath,$SDK_PATH/build/dependency/libkvscproducer/kvscproducer-src \
   -Wl,-rpath,$SDK_PATH/open-source/local/lib \
   -Wl,-rpath,/opt/homebrew/lib \
   -Wl,-rpath,@loader_path/build \
-  \
   -lKinesisVideoProducer -lcproducer -lkvsCommonCurl \
   -lcurl \
-  \
   $(pkg-config --libs libavcodec libavutil libavformat opencv4) \
-  \
   -o kvs_app
 
 # clang++ ../video-streaming-service/camera_2.cpp -std=c++17 \
