ec50864f5782861f119e954c0a7ef42b3a56ddab
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-03-26 00:21:08 +0500

    added consistent spacing for open search random password

--- secrets-manager.tf
+++ secrets-manager.tf
@@ -22,11 +22,11 @@ resource "aws_secretsmanager_secret" "opensearch" {
 
 resource "random_password" "opensearch" {
   length           = 16
-  
-  min_upper   = 1
-  min_lower   = 1
-  min_numeric = 1
-  min_special = 1
+
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
 
   special          = true
   override_special = "!@#%^&*()-_=+"
