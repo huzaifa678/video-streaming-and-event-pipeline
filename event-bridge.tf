5dd67d32470ad287e6423035c1264b788b40cb52
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-03-31 03:43:51 +0500

    removed the duplicate permission resource

--- event-bridge.tf
+++ event-bridge.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_event_rule" "s3_rekognition_trigger" {
     detail-type = ["Object Created"]
     detail = {
       bucket = {
-        name = [aws_s3_bucket.rekognition_raw.id]
+        name = [aws_s3_bucket.rekognition_raw.bucket]
       }
     }
   })
@@ -17,12 +17,4 @@ resource "aws_cloudwatch_event_target" "lambda_target" {
   rule      = aws_cloudwatch_event_rule.s3_rekognition_trigger.name
   target_id = "StartGlueLambda"
   arn       = aws_lambda_function.start_glue.arn
-}
-
-resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.start_glue.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.s3_rekognition_trigger.arn
 }
