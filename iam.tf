0d686d28d07b5f7489393922d7cd67149dcd4d08
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-03-30 21:27:17 +0500

    added new iam role policy for rekognition to access kinesis video stream

--- iam.tf
+++ iam.tf
@@ -366,3 +366,32 @@ resource "aws_iam_role_policy_attachment" "glue_attach" {
   role       = aws_iam_role.glue_role.name
   policy_arn = aws_iam_policy.glue_policy.arn
 }
+
+resource "aws_iam_role_policy" "rekognition_kinesis_access" {
+  name = "${var.project_name}-rekognition-kinesis-access"
+  role = aws_iam_role.rekognition_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "kinesisvideo:GetDataEndpoint",
+          "kinesisvideo:GetMedia",
+          "kinesisvideo:DescribeStream"
+        ]
+        Resource = aws_kinesis_video_stream.video_stream.arn
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "kinesis:PutRecord",
+          "kinesis:PutRecords"
+        ]
+        Resource = aws_kinesis_stream.rekognition_output.arn
+      }
+    ]
+  })
+}
