fbd6435ef23e7b832388361ee0c7c34e6e4aef70
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-06-27 15:49:44 +0500

    remove local exec for kvs start

--- modules/streaming/main.tf
+++ modules/streaming/main.tf
@@ -33,10 +33,7 @@ resource "aws_rekognition_stream_processor" "video_processor" {
     opt_in = false
   }
 
-  provisioner "local-exec" {
-    command = "sleep 20 && aws rekognition start-stream-processor --name ${self.name} --region ${var.aws_region}"
-  }
-
+  # handled by github actions pipeline
   lifecycle {
     create_before_destroy = true
     ignore_changes        = [data_sharing_preference]
